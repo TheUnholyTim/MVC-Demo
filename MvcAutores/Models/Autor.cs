667ab0900b64a8abfbced4697d5071dc9f4b4ef8
Tim <thehelenamaria@gmail.com>
2023-02-02 13:03:46 -0300

    some quality of life changes

--- MvcAutores/Models/Autor.cs
+++ MvcAutores/Models/Autor.cs
@@ -1,11 +1,15 @@
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcAutores.Models;
 
 public class Autor{
     public int Id { get; set; }
     [MaxLength(200)]
+    [Display(Name = "Autor")]
     public string? Nome { get; set; }
+    [Display(Name = "Data de Nascimento")]
 
     [DataType(DataType.Date)]
     public DateTime Dt_nasc { get; set; }
